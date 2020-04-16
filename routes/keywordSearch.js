28e352ef17cb6b9829379c484d32042001e73027
Steve Jadav <55038469+Steve-Jadav@users.noreply.github.com>
2020-04-16 13:43:34 -0700

    Update keywordSearch.js

--- routes/keywordSearch.js
+++ routes/keywordSearch.js
@@ -9,6 +9,12 @@ var inverted_index = [ require('./../public/data/biorxiv_medrxiv/biorxiv_medrxiv
              require('./../public/data/noncomm_use_subset/noncomm_use_subset_IDX.json'),
              require('./../public/data/comm_use_subset/comm_use_subset_IDX.json') ];
 
+var idx = [];
+idx[0] = lunr.Index.load(inverted_index[0]);
+idx[1] = lunr.Index.load(inverted_index[1]);
+idx[2] = lunr.Index.load(inverted_index[2]);
+idx[3] = lunr.Index.load(inverted_index[3]);
+
 /* GET the best match files */
 router.get('/', function(req, res, next) {
 
@@ -17,8 +23,8 @@ router.get('/', function(req, res, next) {
 
   for (var i = 0; i < inverted_index.length; i++) {
     var idx = lunr.Index.load(inverted_index[i]);
-    var result = idx.search(searchTerms);
-    result.slice(0, 14).forEach(function (data) {
+    var result = idx[i].search(searchTerms);
+    result.slice(0, 25).forEach(function (data) {
       bestMatchFiles.push(data.ref);
     });
   }
