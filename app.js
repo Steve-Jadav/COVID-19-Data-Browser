3258a9524cfc1e971dfde2b262f54f122f223f80
Steve Jadav <55038469+Steve-Jadav@users.noreply.github.com>
2020-06-08 12:41:55 -0700

    AWS Kendra integration

--- app.js
+++ app.js
@@ -3,13 +3,19 @@ const cookieParser = require("cookie-parser");
 const createError = require("http-errors");
 const path = require("path");
 
+
 // Initialize the app
 const app = express();
 const port = 3000;
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var fs = require('fs');
+var hit_count = 0;
 
 var summarizeRouter = require("./routes/summarize");
 var textViewRouter = require("./routes/textViewer");
-var keywordSearchRouter = require("./routes/keywordSearch");
+var kendraRouter = require("./routes/kendra");
+//var keywordSearchRouter = require("./routes/keywordSearch");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -19,6 +25,7 @@ app.use(express.static(path.join(__dirname, "public")));
 // Routes
 app.use("/summarize", summarizeRouter);
 app.use("/textViewer", textViewRouter);
+app.use("/kendra", kendraRouter);
 app.use("/keywordSearch", keywordSearchRouter);
 
 // Error route
@@ -26,7 +33,13 @@ app.use(function (req, res, next) {
   res.status(404).send("Unable to locate the requested page. Please recheck the url.");
 });
 
+io.on('connection', (socket) => {
+  hit_count += 1;
+  var hits = { "website-hits": hit_count };
+  fs.writeFileSync("website-hits.json", JSON.stringify(hits));
+  console.log("User connected");
+});
 
-app.listen(port, function () {
+http.listen(port, function () {
   console.log(`Application listening on port ${port}!`);
 });
